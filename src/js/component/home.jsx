f672da6009405776f50f7364dddfe9469f67d3fb
Txl3r <Harvey.Tyler@live.jobcorps.org>
2022-04-07 19:10:26 +0000

    finished

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,13 +2,39 @@ import React, { useState } from "react";
 import "../../styles/index.css";
 
 const Home = () => {
-	const [myColor, setMyColor] = useState("");
+	const [myColor, setMyColor] = useState("brown");
 
 	return (
-		<div className="traffic-light">
-			<div className="red-light circle"></div>
-			<div className="yellow-light circle"></div>
-			<div className="green-light circle"></div>
+		<div>
+			<div className={"traffic-light"}>
+				<div
+					className={
+						myColor == "red"
+							? "box-shadow red-light circle"
+							: "red-light circle"
+					}
+					onClick={() => {
+						setMyColor("red");
+					}}></div>
+				<div
+					className={
+						myColor == "yellow"
+							? "box-shadow yellow-light circle"
+							: "yellow-light circle"
+					}
+					onClick={() => {
+						setMyColor("yellow");
+					}}></div>
+				<div
+					className={
+						myColor == "green"
+							? "box-shadow green-light circle"
+							: "green-light circle"
+					}
+					onClick={() => {
+						setMyColor("green");
+					}}></div>{" "}
+			</div>
 		</div>
 	);
 };
